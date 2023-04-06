8a8cfdda2364d14e307dccc0a761dc59cb89efdf
Kristýna Nováková <novakovkri@gmail.com>
2023-04-06 18:03:38 +0200

    button querySelector

---
+++ hra.js
@@ -0,0 +1,27 @@
+let currentPlayer = 'circle';
+
+const button1 = document.querySelector('.playing-field button:nth-child(1)');
+const button2 = document.querySelector('.playing-field button:nth-child(2)');
+const button3 = document.querySelector('.playing-field button:nth-child(3)');
+const button4 = document.querySelector('.playing-field button:nth-child(4)');
+const button5 = document.querySelector('.playing-field button:nth-child(5)');
+const button6 = document.querySelector('.playing-field button:nth-child(6)');
+const button7 = document.querySelector('.playing-field button:nth-child(7)');
+const button8 = document.querySelector('.playing-field button:nth-child(8)');
+const button9 = document.querySelector('.playing-field button:nth-child(9)');
+const button10 = document.querySelector('.playing-field button:nth-child(10)');
+
+const handleButtonClick = (event) => {
+  button.
+}
+
+button1.addEventListener('click');
+button2.addEventListener('click');
+button3.addEventListener('click');
+button4.addEventListener('click');
+button5.addEventListener('click');
+button6.addEventListener('click');
+button7.addEventListener('click');
+button8.addEventListener('click');
+button9.addEventListener('click');
+button10.addEventListener('click');
