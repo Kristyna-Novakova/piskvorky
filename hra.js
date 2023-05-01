1a6d8e0666303a39d4ff7563a8a081487b123f13
Kristýna Nováková <novakovkri@gmail.com>
2023-05-01 20:46:42 +0200

    testing disabled property

--- hra.js
+++ hra.js
@@ -57,32 +57,34 @@ const handleButtonClick = (event) => {
   if (currentPlayer === 'cross') {
     buttonAll.forEach((btn) => (btn.disabled = true));
 
-    fetch('https://piskvorky.czechitas-podklady.cz/api/suggest-next-move', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        board: board,
-        player: 'x',
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const { x, y } = data.position;
-        const index = x + y * 10; // Konvertování x a y na index pro 10x10 board
-        const field = buttonAll[index];
-        field.click();
-      });
+    setTimeout(() => {
+      fetch('https://piskvorky.czechitas-podklady.cz/api/suggest-next-move', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          board: board,
+          player: 'x',
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const { x, y } = data.position;
+          const index = x + y * 10; // Konvertování x a y na index pro 10x10 board
+          const field = buttonAll[index];
+          field.click();
+        });
 
-    buttonAll.forEach((btn) => {
-      if (
-        !btn.classList.contains('board__field--circle') &&
-        !btn.classList.contains('board__field--cross')
-      ) {
-        btn.disabled = false; // Povolení tlačítek, na kterých nebyl označen křížek ani kolečko
-      }
-    });
+      buttonAll.forEach((btn) => {
+        if (
+          !btn.classList.contains('board__field--circle') &&
+          !btn.classList.contains('board__field--cross')
+        ) {
+          btn.disabled = false; // Povolení tlačítek, na kterých nebyl označen křížek ani kolečko
+        }
+      });
+    }, 2000);
   }
 };
 
